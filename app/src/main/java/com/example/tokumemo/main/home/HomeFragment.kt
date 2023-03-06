aba5e38cabfb793fd251fc4841824305de7d9d3b
akidon0000 <a@art.jp>
2023-03-06 20:28:40 +0900

    [Add]pr画面表示成功

--- app/src/main/java/com/example/tokumemo/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/tokumemo/main/home/HomeFragment.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.findFragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tokumemo.GetImage
@@ -44,6 +49,7 @@ class HomeFragment : Fragment() {
 
     }
 
+
     private fun listViewInitSetting(view: View) {
 
         // xmlにて実装したListViewの取得
@@ -77,32 +83,22 @@ class HomeFragment : Fragment() {
         listView.adapter = adapter
     }
 
-//    private fun apiCommunicatingInitSetting(view: View) {
-//
-//        // 広告画像貼り付け
-//        if (encryptedLoad("imageNum") != ""){
-//            imageNum = encryptedLoad("imageNum").toInt()
-//        }
-//        val imageButton = findViewById<ImageButton>(R.id.image)
-//
-//        getAd()
-//
-//        imageButton.setOnClickListener{
-//            if (adExistence){
-//                goWeb(adURL)
-//            }
-//        }
-//    }
-
     /// 広告を一定時間ごとに読み込ませる処理のスイッチ
     private fun adImagesRotationTimerON(view: View) {
+        val imageView = view.findViewById<ImageView>(R.id.pr_image_button)
+
+        // ImageViewにクリックリスナーを設定
+        imageView.setOnClickListener {
+            val childFragment = PRFragment()
+            val transaction = childFragmentManager.beginTransaction()
+            transaction.replace(R.id.child_fragment_container, childFragment).commit()
+        }
 
         // 5000 ms 毎に実行
         Timer().scheduleAtFixedRate(0, 5000) {
             val num = viewModel.selectPRImageNumber()
             if (num != null) {
-                val imageButton = view.findViewById<ImageView>(R.id.pr_image_button)
-                val imageTask: GetImage = GetImage(imageButton)
+                val imageTask: GetImage = GetImage(imageView)
                 imageTask.execute(viewModel.prItems[num].imageURL)
             }
         }
---
+++ app/src/main/java/com/example/tokumemo/main/home/PRFragment.kt
@@ -0,0 +1,22 @@
+package com.example.tokumemo.main.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tokumemo.R
+
+class PRFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_pr, container, false)
+
+        return view
+
+    }
+}
