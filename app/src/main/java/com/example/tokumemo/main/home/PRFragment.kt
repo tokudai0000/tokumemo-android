ff950f33a724c1584c817858de62a6dba61c837f
akidon0000 <a@art.jp>
2023-03-06 23:58:58 +0900

    [Add]PR画像フラグメントを消去可能に

--- app/src/main/java/com/example/tokumemo/main/home/PRFragment.kt
+++ app/src/main/java/com/example/tokumemo/main/home/PRFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.example.tokumemo.R
 
@@ -15,6 +16,23 @@ class PRFragment : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_pr, container, false)
+
+        val backButton = view.findViewById<ImageButton>(R.id.back_button)
+        backButton.setOnClickListener {
+            // 子フラグメントマネージャーを取得
+            val childFragmentManager = parentFragment?.childFragmentManager
+
+            // 子フラグメントを終了する
+            if (childFragmentManager != null) {
+                val childFragment = childFragmentManager.findFragmentById(R.id.child_fragment_container)
+                if (childFragment != null) {
+                    childFragmentManager.beginTransaction()
+                        .remove(childFragment)
+                        .commit()
+                }
+            }
+        }
+
 
         return view
 
