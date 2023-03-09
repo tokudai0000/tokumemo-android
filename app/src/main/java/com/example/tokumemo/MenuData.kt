116b91a461687236cf7122e659df8b5211b2d6b2
akidon0000 <a@art.jp>
2023-03-09 11:36:47 +0900

    [Fix]initMenuListを修正

--- app/src/main/java/com/example/tokumemo/MenuData.kt
+++ app/src/main/java/com/example/tokumemo/MenuData.kt
@@ -1,36 +1,36 @@
 package com.example.tokumemo
 
 enum class MenuListItemType {
-    courseManagementHomePC,         // 教務事務システム
-    courseManagementHomeMobile,
-    manabaHomePC,                   // マナバ
-    manabaHomeMobile,
-    portal,                         // 統合認証ポータル
-    libraryWebHomePC,               // 図書館Webサイト常三島
-    libraryWebHomeKuraPC,           // 図書館Webサイト蔵本
-    libraryWebHomeMobile,
-    libraryMyPage,                  // 図書館MyPage
-    libraryBookLendingExtension,    // 図書館本貸出し期間延長
-    libraryBookPurchaseRequest,     // 図書館本購入リクエスト
-    libraryCalendar,                // 図書館カレンダー
-    syllabus,                       // シラバス
-    timeTable,                      // 時間割
-    currentTermPerformance,         // 今年の成績表
-    termPerformance,                // 成績参照
-    presenceAbsenceRecord,          // 出欠記録
-    classQuestionnaire,             // 授業アンケート
-    mailService,                    // メール
-    careerCenter,                   // キャリアセンター
-    coopCalendar,                   // 徳島大学生活共同組合
-    cafeteria,                      // 徳島大学食堂
-    courseRegistration,             // 履修登録
-    systemServiceList,              // システムサービス一覧
-    eLearningList,                  // Eラーニング一覧
-    universityWeb,                  // 大学サイト
-    studySupportSpace,              // 学びサポート企画部
-    disasterPrevention,             // 上月研究室防災情報
+    CourseManagementHomePC,         // 教務事務システム
+    CourseManagementHomeMobile,
+    ManabaHomePC,                   // マナバ
+    ManabaHomeMobile,
+    Portal,                         // 統合認証ポータル
+    LibraryWebHomePC,               // 図書館Webサイト常三島
+    LibraryWebHomeKuraPC,           // 図書館Webサイト蔵本
+    LibraryWebHomeMobile,
+    LibraryMyPage,                  // 図書館MyPage
+    LibraryBookLendingExtension,    // 図書館本貸出し期間延長
+    LibraryBookPurchaseRequest,     // 図書館本購入リクエスト
+    LibraryCalendar,                // 図書館カレンダー
+    Syllabus,                       // シラバス
+    TimeTable,                      // 時間割
+    CurrentTermPerformance,         // 今年の成績表
+    TermPerformance,                // 成績参照
+    PresenceAbsenceRecord,          // 出欠記録
+    ClassQuestionnaire,             // 授業アンケート
+    MailService,                    // メール
+    CareerCenter,                   // キャリアセンター
+    CoopCalendar,                   // 徳島大学生活共同組合
+    Cafeteria,                      // 徳島大学食堂
+    CourseRegistration,             // 履修登録
+    SystemServiceList,              // システムサービス一覧
+    ELearningList,                  // Eラーニング一覧
+    UniversityWeb,                  // 大学サイト
+    StudySupportSpace,              // 学びサポート企画部
+    DisasterPrevention,             // 上月研究室防災情報
 
-    favorite,                       // お気に入り
+    Favorite,                       // お気に入り
 }
 
 data class MenuData(
