22318594a1274bdb2ed687c6094fc42400110a77
akidon0000 <a@art.jp>
2024-01-02 20:35:40 +0900

    [67] ClubListを修正

--- app/src/main/java/com/example/tokumemo/ui/clublists/ClubListsFragment.kt
+++ app/src/main/java/com/example/tokumemo/ui/clublists/ClubListsFragment.kt
@@ -2,13 +2,13 @@ package com.example.tokumemo.ui.clublists
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.JsResult
 import android.webkit.WebChromeClient
 import android.webkit.WebView
+import androidx.fragment.app.Fragment
 import com.example.tokumemo.R
 import com.example.tokumemo.common.Url
 import com.example.tokumemo.ui.web.WebActivity
@@ -22,10 +22,11 @@ class ClubListsFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_club_lists, container, false)
         val webView = view.findViewById<WebView>(R.id.club_lists_web_view)
+        // 有効にすることでHTML側からネイティブ側へアクションを起こしている
         webView.settings.javaScriptEnabled = true
         webView.loadUrl(Url.ClubList.urlString)
 
-        webView.setWebChromeClient(object : WebChromeClient() {
+        webView.webChromeClient = object : WebChromeClient() {
             // Web側(HTML,JavaScript)からKotlinが通知を受け、WebActivityを表示させる
             override fun onJsAlert(view: WebView, url: String, message: String, result: JsResult): Boolean {
                 // アラートの表示をキャンセルする
@@ -37,7 +38,7 @@ class ClubListsFragment : Fragment() {
 
                 return true
             }
-        })
+        }
         return view
     }
 }
