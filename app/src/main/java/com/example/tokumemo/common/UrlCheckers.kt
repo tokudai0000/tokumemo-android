b720fa7b4a6cc86d24cf2630df365d3ea771e222
akidon0000 <a@art.jp>
2024-01-06 15:45:46 +0900

    [78]webログイン処理を実装

---
+++ app/src/main/java/com/example/tokumemo/common/UrlCheckers.kt
@@ -0,0 +1,69 @@
+package com.example.tokumemo.common
+
+object UrlCheckers {
+
+    enum class UrlType {
+        UniversityLogin,      // 大学のログインページ
+        OutlookLoginForm,     // Outlookのログインフォーム
+        TokudaiCareerCenter   // 徳大キャリアセンター
+    }
+
+    /// 指定されたURLにJavaScriptを挿入できるかどうかを判断します。
+    /// - Parameters:
+    ///   - urlString: 確認するURLの文字列
+    ///   - canExecuteJavascript: JavaScriptの実行が許可されているかどうかのフラグ
+    ///   - urlType: 確認するURLのタイプ
+    /// - Returns: JavaScriptを挿入できる場合は`true`、そうでない場合は`false`
+    fun shouldInjectJavaScript(urlString: String, canExecuteJavascript: Boolean, urlType: UrlType): Boolean {
+        if (!canExecuteJavascript) return false
+        return when (urlType) {
+            UrlType.UniversityLogin -> urlString.contains(Url.UniversityLogin.urlString)
+            UrlType.OutlookLoginForm -> urlString.contains(Url.OutlookLoginForm.urlString)
+            UrlType.TokudaiCareerCenter -> urlString.contains(Url.TokudaiCareerCenter.urlString)
+        }
+    }
+
+    /// URLが大学サイトのアンケート催促画面かを確認します。
+    /// - Parameter urlString: 確認するURLの文字列
+    /// - Returns: アンケート催促のURLである場合は`true`、そうでない場合は`false`
+    fun isSkipReminderURL(urlString: String): Boolean {
+        return urlString.contains(Url.SkipReminder.urlString)
+    }
+
+    /// URLが大学サイトのタイムアウト画面かを確認します。(2通り存在)
+    fun isUniversityServiceTimeoutURL(urlStr: String): Boolean {
+        return urlStr == Url.UniversityServiceTimeOut.urlString || urlStr == Url.UniversityServiceTimeOut2.urlString
+    }
+
+    /// URLが大学サイトのログインに失敗し、再入力を求められている画面かを確認します。
+    /// 初回ログイン https://localidp.ait230.tokushima-u.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=e1s1
+    /// 1回ログイン失敗 https://localidp.ait230.tokushima-u.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=e1s2
+    /// 2回ログイン失敗 https://localidp.ait230.tokushima-u.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=e1s3
+    /// - Parameter urlStr: 確認するURLの文字列
+    /// - Returns: ログインに失敗した際のURLである場合は`true`、そうでない場合は`false`
+    fun isFailureUniversityServiceLoggedInURL(urlStr: String): Boolean {
+        if (urlStr.contains("https://localidp.ait230.tokushima-u.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=")) {
+            val start = urlStr.length - 2
+            // 下2桁を比較
+            if (urlStr.substring(start) != "s1") {
+                return true
+            }
+        }
+        return false
+    }
+
+    /// URLが大学サイトのログイン直後かを確認します。(3通り存在)
+    /// 1, アンケート最速画面
+    /// 2, 教務事務システムのモバイル画面(iPhone)
+    /// 3, 教務事務システムのPC画面(iPad)
+    /// - Parameter urlStr: 確認するURLの文字列
+    /// - Returns: ログイン直後のURLである場合は`true`、そうでない場合は`false`
+    fun isImmediatelyAfterLoginURL(urlStr: String): Boolean {
+        val targetURLs = listOf(Url.SkipReminder.urlString,
+            Url.CourseManagementMobile.urlString,
+            Url.CourseManagementPC.urlString)
+        return targetURLs.contains(urlStr)
+    }
+}
+
+// Url クラスと Url.string() メソッドは、Swiftコードで参照されているものと同じ機能を持つようにKotlinで定義する必要があります。
--- app/src/main/java/com/example/tokumemo/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/tokumemo/ui/splash/SplashActivity.kt
@@ -1,18 +1,30 @@
 package com.example.tokumemo.ui.splash
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.Gravity
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.tokumemo.R
+import com.example.tokumemo.common.Url
+import com.example.tokumemo.common.UrlCheckers
 import com.example.tokumemo.data.DataManager
+import com.example.tokumemo.data.DataManager.Companion.canExecuteJavascript
+import com.example.tokumemo.ui.MainActivity
 import com.example.tokumemo.ui.agreement.AgreementActivity
+import com.example.tokumemo.ui.pr.PublicRelationsActivity
+import com.example.tokumemo.ui.web.WebActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class SplashActivity : AppCompatActivity() {
@@ -21,9 +33,70 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        configureWebView()
         configureLoginStatus()
         configureCopylight()
+    }
 
+    private fun configureWebView() {
+        val webView: WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl(Url.UniversityTransitionLogin.urlString)
+
+        webView.webViewClient = object : WebViewClient() {
+
+            // Url.UniversityTransitionLoginは強制で外部検索エンジンが開かれるので、ここで禁止している
+            // 外部ブラウザで開かずにWebView内で処理する
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+
+
+            // URLの読み込み開始
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                // アンラップ
+                url ?: return
+
+                // タイムアウト判定
+                if (UrlCheckers.isUniversityServiceTimeoutURL(url)) {
+                    webView.loadUrl(Url.UniversityTransitionLogin.urlString)
+                }
+
+                // ログイン成功
+                if (UrlCheckers.isImmediatelyAfterLoginURL(url)) {
+                    val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+                // ログイン失敗
+                if (UrlCheckers.isFailureUniversityServiceLoggedInURL(url)) {
+                    val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+            // 読み込み完了
+            override fun onPageFinished(view: WebView?, url: String?) {
+                // アンラップ
+                url ?: return
+
+                // ログイン処理を行うURLか判定
+                if (UrlCheckers.shouldInjectJavaScript(url, canExecuteJavascript, urlType = UrlCheckers.UrlType.UniversityLogin)) {
+                    canExecuteJavascript = false
+                    val cAccount = getPassword(view!!.context,"KEY_cAccount")
+                    val password = getPassword(view!!.context,"KEY_password")
+                    webView.evaluateJavascript("document.getElementById('username').value= '$cAccount'", null)
+                    webView.evaluateJavascript("document.getElementById('password').value= '$password'", null)
+                    webView.evaluateJavascript("document.getElementsByClassName('form-element form-button')[0].click();", null)
+                }
+
+                super.onPageFinished(view, url)
+            }
+        }
     }
 
     private fun configureLoginStatus() {
