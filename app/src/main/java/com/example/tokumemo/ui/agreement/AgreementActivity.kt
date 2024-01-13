4d521627eac7521bfa3fdf2077c9e6973ae8c1b9
akidon0000 <a@art.jp>
2024-01-13 17:11:04 +0900

    [82] 規約の内容をAPIから取得

--- app/src/main/java/com/example/tokumemo/ui/agreement/AgreementActivity.kt
+++ app/src/main/java/com/example/tokumemo/ui/agreement/AgreementActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tokumemo.data.DataManager
 import com.example.tokumemo.R
@@ -15,6 +16,12 @@ import com.example.tokumemo.domain.model.AdItem
 import com.example.tokumemo.ui.RootActivity
 import com.example.tokumemo.ui.splash.SplashActivity
 import com.example.tokumemo.ui.web.WebActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.URL
 
 class AgreementActivity : AppCompatActivity() {
 
@@ -29,6 +36,16 @@ class AgreementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agreement)
+
+        val termText: TextView = findViewById(R.id.agreement_text_view)
+
+        GlobalScope.launch {
+            try {
+                termText.text = getTermText()
+            } catch (e: Exception) {
+                println("Error occurred: ${e.message}")
+            }
+        }
 
         findViewById<Button>(R.id.terms_button).setOnClickListener {
             val intent = Intent(this, WebActivity::class.java)
@@ -52,6 +69,23 @@ class AgreementActivity : AppCompatActivity() {
             returnIntent.putExtra(RootActivity.EXTRA_NEXT_ACTIVITY, "MainActivity")
             setResult(Activity.RESULT_OK, returnIntent)
             finish()
+        }
+    }
+
+    private suspend fun getTermText(): String {
+        val apiUrl = "https://tokudai0000.github.io/tokumemo_resource/api/v1/term_text.json"
+
+        return withContext(Dispatchers.IO) {
+            try {
+                val responseData = URL(apiUrl).readText()
+                val jsonData = JSONObject(responseData)
+
+                // Jsonデータから内容物を取得
+                jsonData.getString("termText")
+            } catch (e: Exception) {
+                // エラー処理、適宜エラーメッセージを返す
+                "Error: ${e.message}"
+            }
         }
     }
 }
