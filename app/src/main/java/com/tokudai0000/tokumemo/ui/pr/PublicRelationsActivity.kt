d9356ed0f5cd5aa91c5988ba0eea7993071d2942
akidon0000 <a@art.jp>
2024-01-17 11:55:27 +0900

    [83] PR修正

--- app/src/main/java/com/tokudai0000/tokumemo/ui/pr/PublicRelationsActivity.kt
+++ app/src/main/java/com/tokudai0000/tokumemo/ui/pr/PublicRelationsActivity.kt
@@ -12,17 +12,18 @@ import com.tokudai0000.tokumemo.R
 import com.tokudai0000.tokumemo.domain.model.AdItem
 import com.tokudai0000.tokumemo.ui.web.WebActivity
 
-// PR画面
-class PublicRelationsActivity : AppCompatActivity() {
-
-    lateinit var item: AdItem
+class PublicRelationsActivity : AppCompatActivity(R.layout.activity_public_relations) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        item = intent.getSerializableExtra(WebActivity.KEY_URL) as AdItem
+        val item = intent.getSerializableExtra(WebActivity.KEY_URL) as AdItem
 
-        setContentView(R.layout.activity_public_relations)
+        val imageView = findViewById<ImageView>(R.id.pr_image_view)
+        GetImage(imageView).execute(item.imageUrlStr)
+
+        val textView = findViewById<TextView>(R.id.textView5)
+        textView.text = item.imageDescription
 
         val backButton = findViewById<ImageButton>(R.id.back_button)
         backButton.setOnClickListener {
@@ -35,10 +36,5 @@ class PublicRelationsActivity : AppCompatActivity() {
             intent.putExtra(WebActivity.KEY_URL, item.targetUrlStr)
             startActivity(intent)
         }
-
-        val imageView = findViewById<ImageView>(R.id.pr_image_view)
-        GetImage(imageView).execute(item.imageUrlStr)
-
-        findViewById<TextView>(R.id.textView5).text = item.imageDescription
     }
 }
